55042492c5cdaaa9493b0b22ae44024666475f81
srinidhiai01 <srinidhiai01@gmail.com>
2024-01-06 12:34:13 +0530

    added main.c

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 #include"forloop.c"
 #include"switch.c"
+#include"table.c"
 #include"twonum.c"
 
 int loop1();
 int operator();
 int twonum();
+int table();
 
 int main()
 {
   loop1();
   operator();
   twonum();
+  table();
  return 0;
 }
