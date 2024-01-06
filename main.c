0f8e55c5cba6a4a49022f5fa46cd01e9af41b442
srinidhiai01 <srinidhiai01@gmail.com>
2024-01-06 12:12:59 +0530

    added file

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"forloop.c"
+#include"switch.c"
+#include"twonum.c"
+
+int loop1();
+int operator();
+int twonum();
+
+int main()
+{
+  loop1();
+  operator();
+  twonum();
+ return 0;
+}
